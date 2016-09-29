91af8c3d25953654675b7a140b6063b4e96a5656
Shimshon Fishler <shimshon.fishler@gmail.com>
2016-09-29 10:41:58 +0300

    Update Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ShowPath
+# ShowPath (for Windows)
+Display the folders in your PATH environment variable, one in each row
+
+Simple
+Shimshon
+
---
+++ ShowPath/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paths = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process);
+
+            var pathArray = paths.Split(';');
+            var i = 1;
+
+            foreach(var p in pathArray)
+            {
+                Console.WriteLine($"{i}: {p}");
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
